18b628a46c18171fafea89926e3ed53598ef256d
shanaglazer <shanaglazer1@gmail.com>
2023-02-06 11:30:50 +0200

    initia commit

---
+++ WindowsFormUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUWindowsFormFramework
+{
+    public class WindowsFormUtility
+    {
+        public static void SetListBinding(ComboBox lst, DataTable dt, string tablename)
+        {
+            lst.DataSource = dt;
+            lst.ValueMember = tablename + "id";
+            lst.DisplayMember = lst.Name.Substring(3);
+            lst.DataBindings.Add("SelectedValue", dt, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        public static void SetControlBinding(Control ctrl, DataTable dt)
+        {
+            string propertyname = "";
+            string controlname = ctrl.Name.ToLower();
+            string columnname = controlname.Substring(3);
+            string controltype = controlname.Substring(0, 3);
+
+            switch (controltype)
+            {
+                case "txt":
+                case "lbl":
+                    propertyname = "Text";
+                    break;
+                case "dtp":
+                    propertyname = "Value";
+                    break;
+            }
+
+            if (propertyname != "" && columnname != "")
+            {
+                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+            }
+        }
+
+        public static void FormatGrid(DataGridView grid)
+        {
+            grid.AllowUserToAddRows = false;
+            grid.ReadOnly = true;
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+    }
+}
